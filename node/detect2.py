b15765089c1cb93608b5b51f5ddda725a742972f
Sameer Soni <indomitablesameer@gmail.com>
2022-12-10 02:46:07 +0100

    introducing picam frame and class

--- node/detect2.py
+++ node/detect2.py
@@ -1,18 +1,50 @@
 import torch
+import picamera2
+import time
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='./best.pt')
+class ratDetection:
 
-def setModelConfig():
-    model.conf = 0.50
+    def __init__(self, captureIndex, modelWeight, device):
+        self.device = device
+        self.captureIndex = captureIndex
+        self.model = self.loadModel(modelWeight)
+    
+    def loadModel(self, modelWeight):
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path=modelWeight)
+        return model
 
-def runDetection(img):
-    results = model(img)
-    results.print()
-    results.save()
+    def setModelConfig(self, confidance=0.4):
+        self.confidance = confidance
+
+    def get_camera_feed():
+        picam = Picamera2()
+        picam.start()
+        return picam
+
+    def runDetection(self, frame):
+        self.model.to(self.device)
+        results = self.model(frame)
+        results.print()
+        results.save()
+        return results
+    
+    def __call__(self):
+        cam = self.get_camera_feed()
+        time.sleep(1)
+
+        while True:
+            frame = cam.capture_array()
+            results = self.runDetection(frame)
+            break
+        cam.stop()
+
+
 
 def main():
     im = './test_data/test.jpg'
-    runDetection(im)
+    #runDetection(im)
+    det = ratDetection(captureIndex=0,modelWeight="best.pt", device="cpu")
+    det()
 
 if __name__ == "__main__":
     main()
