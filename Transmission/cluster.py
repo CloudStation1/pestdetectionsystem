bb5bb2973e892e7eb77622572c49f712f24c6ccc
Jatinkumar <nakranijatinkumar@gmail.com>
2022-12-23 17:39:07 +0100

    convert normal functions to classes function

--- Transmission/cluster.py
+++ Transmission/cluster.py
@@ -1,53 +1,42 @@
-import random
 import json
 import base64
-from paho.mqtt import client as mqtt_client
+from paho.mqtt import client as mqtt
 
+class Cluster:
+    broker = '192.168.31.219'
+    port = 1883
+    topic = 'rat/img'    
 
-broker = '192.168.31.219'
-port = 1883
-topic = "data/file"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
-username = 'client'
-password = 'test'
+    def getMQTTConnection(self):
+        client = mqtt.Client()
+        client.on_connect = self.on_connect
+        client.connect(self.broker, self.port)
+        return client
 
-
-def connect_mqtt() -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker!")
+            print('Connected to Broker ' + self.broker)
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+    def subscribe(self):
+        client = self.getMQTTConnection()
+        client.subscribe(self.topic)
+        client.on_message = self.on_message
+        client.loop_forever()
 
+    def on_message(self, client, userdata, msg):
+        try:
+            if (msg.topic == self.topic):
+                dest_path = 'processImg/'
+                m_in = json.loads(str(msg.payload.decode('utf-8','ignore')))
+                img_name = m_in['img_name']
+                image_byte = base64.b64decode(m_in['img_content'])
+                f = open(dest_path + img_name, 'wb')
+                f.write(image_byte)
+                print('Image('+ img_name +') Receive Sucessfully.')
+        except Exception as e:
+            print(e)
 
-def subscribe(client: mqtt_client):
-    def on_message(client, userdata, msg):
-        #print(msg.topic)
-        m_decode = str(msg.payload.decode('utf-8','ignore'))
-        m_in = json.loads(m_decode)
-        dest_path = 'processImg/'
-        img_name = m_in['img_name']
-        image_byte = base64.b64decode(m_in['img_content'])
-        f = open(dest_path + img_name, 'wb')
-        f.write(image_byte)
-        print('Image Receive Sucessfully.')
-
-    client.subscribe(topic)
-    client.on_message = on_message
-
-
-def run():
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
-
-
-if __name__ == '__main__':
-    run()
+cl = Cluster()
+cl.subscribe()
--- Transmission/node.py
+++ Transmission/node.py
@@ -1,56 +1,77 @@
-import random
-import time
 import base64
 import json
-from paho.mqtt import client as mqtt_client
+import os.path as path
+from paho.mqtt import client as mqtt
 
+class Node:
+    broker = '192.168.31.219'
+    port = 1883
+    topic = 'rat/img'
 
-broker = '192.168.31.219'
-port = 1883
-topic = "data/file"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
-username = 'client'
-password = 'test'
+    def __init__(self, img=''):
+        self.img = img
 
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
+    def getMQTTConnection(self):
+        try:
+            client = mqtt.Client()
+            client.on_connect = self.on_connect
+            client.connect(self.broker, self.port)
+            return client
+        except Exception as e:
+            print(e)
+
+    def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
-            print(flags)
-            print("Connected to MQTT Broker!")
+            print('Connected to Broker ' + self.broker)
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+    def getImageName(self):
+        return path.basename(self.img)
 
-def prepareJsonImg(imgName):
-    imgPath = 'images/'
-    f = open(imgPath + imgName, 'rb')
-    image_byte = base64.b64encode(f.read())
-    image_str = image_byte.decode('ascii')  # byte to str
-    data = {
-        'img_path' : imgPath,
-        'img_name' : imgName,
-        'img_content' : image_str     
-    }
-    return data
+    def isImgFound(self):
+        if path.exists(self.img):
+            return 1
+        else:
+            return 0
 
-def publishMsg(client):
-    time.sleep(1)
-    imgArray = ['IMG_5981.jfif', 'IMG_5982.jfif', 'IMG_5983.jfif', 'IMG_5984.jfif', 'IMG_5985.jfif', 'IMG_5986.jfif', 'IMG_5987.jfif', 'IMG_5988.jfif', 'IMG_5989.jfif', 'IMG_5990.jfif']
-    for imgName in imgArray:
-        img = prepareJsonImg(imgName)
-        jmod = json.dumps(img)
-        result = client.publish(topic, jmod)
-        print(result)
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publishMsg(client)
+    def checkResult(self,result):
+        if (result == 0):
+            print('Image('+ self.getImageName() +') Sent Sucessfully.')
+        else:
+            print('Image('+ self.getImageName() +') Can not send.')
 
-if __name__ == '__main__':
-    run()
+    def prepareJsonImg(self):
+        try:
+            if not self.isImgFound():
+                raise Exception("Image not Found.")
+            f = open(self.img, 'rb')
+            image_byte = base64.b64encode(f.read())
+            image_str = image_byte.decode('ascii')  # byte to str
+            data = {
+                'img_name' : self.getImageName(),
+                'img_content' : image_str
+            }
+            return json.dumps(data)
+        except Exception as e:
+            print(e)
+
+    def transferData(self):
+        try:
+            if not self.isImgFound():
+                raise Exception("Image not Found.")
+
+            client = self.getMQTTConnection()
+            client.loop_start()
+            preparedImg = self.prepareJsonImg()
+            result, count = client.publish(self.topic, preparedImg)
+            self.checkResult(result)
+            client.loop_stop()
+        except Exception as e:
+            print(e)
+
+# imgArray = ['IMG_5981.jfif', 'IMG_5982.jfif', 'IMG_5983.jfif', 'IMG_5984.jfif', 'IMG_5985.jfif', 'IMG_5986.jfif', 'IMG_5987.jfif', 'IMG_5988.jfif', 'IMG_5989.jfif', 'IMG_5990.jfif']
+imgArray = ['IMG_5981.jfif']
+for img in imgArray:
+    p1 = Node('images/'+img);
+    p1.transferData()
