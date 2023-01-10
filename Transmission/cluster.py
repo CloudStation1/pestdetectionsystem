18770cc1624fb45507f978dd91f3de1ffed9dee2
Sameer Soni <indomitablesameer@gmail.com>
2023-01-10 23:17:03 +0100

    logging fix, correction to mqtt and minio ip port

--- Transmission/cluster.py
+++ Transmission/cluster.py
@@ -4,6 +4,12 @@ import os
 import io
 from commenFunc import CommenFunc
 from minio import Minio
+import uuid
+import logging
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 class Cluster(CommenFunc):
 
@@ -12,21 +18,21 @@ class Cluster(CommenFunc):
         client.subscribe(self.getTopic())
         client.on_message = self.onMessage
         client.loop_forever()
+        log.info('subscribed to topic:' + self.getTopic())
 
     def onMessage(self, client, userdata, msg):
         try:
+            log.info('mqtt msg received')
             if (msg.topic == self.getTopic()):
                 client = self.getMinIO()
                 if client.bucket_exists(self.bucketName):
-                    client.put_object(self.bucketName, self.getJsonFileName(msg.payload), io.BytesIO(msg.payload), len(msg.payload), content_type = self.contentType)
-                    print('File uploaded to Minio')
+                    client.put_object(self.bucketName, self.getJsonFileName(), io.BytesIO(msg.payload), len(msg.payload), content_type = self.contentType)
+                    log.info('File uploaded to Minio')
         except Exception as e:
-            print(e)
+            log.exception('exception occured')
 
-    def getJsonFileName(self, payload):
-        m_in = json.loads(str(payload.decode('utf-8','ignore')))
-        filename, file_extension = os.path.splitext(m_in['img_name'])
-        return filename + '(' + m_in['today_datetime'] + ').json'
+    def getJsonFileName(self):
+        return uuid.uuid4().hex + '.json'
 
     def getMinIO(self):
         return Minio(
@@ -43,7 +49,12 @@ class Cluster(CommenFunc):
         self.bucketName = bucketName
         self.contentType = contentType
 
-# cl = Cluster('192.168.31.219', 1883)
-# cl.setTopic('detect/rat')
-# cl.setMinIoConfig('localhost:9000','78SHN5mbnrYYoOy2','as1QQn2Gnhepvx3E2tJ6vZLnUGVkZmLY','cloud','application/json')
-# cl.subscribe()
+def main():
+    log.debug('mqttreceiver started..')
+    cl = Cluster('192.168.178.38', 32267)
+    cl.setTopic('detect/rat')
+    cl.setMinIoConfig('192.168.178.38:30544','SvgpyrBtHEp5QEXe','AJess9U1cPLxqejFkDGikM4Is1ftie0e','pestdetection','application/json')
+    cl.subscribe()
+
+if __name__ == "__main__":
+    main()
--- Transmission/commenFunc.py
+++ Transmission/commenFunc.py
@@ -1,4 +1,9 @@
 from paho.mqtt import client as mqtt
+import logging
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 class CommenFunc:
 
@@ -19,10 +24,10 @@ class CommenFunc:
 			client.connect(self.brokerId, self.port)
 			return client
 		except Exception as e:
-			print(e)
+			log.exception('exception occured')
 
 	def on_connect(self, client, userdata, flags, rc):
 		if rc == 0:
-			print('Connected to Broker ' + self.brokerId)
+			log.info('Connected to Broker ' + self.brokerId)
 		else:
-			print("Failed to connect, return code %d\n", rc)
+			log.error("Failed to connect, return code %d\n", rc)
