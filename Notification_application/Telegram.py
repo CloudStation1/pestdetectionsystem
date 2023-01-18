5e84ebb4ef5c7c4dd62578b7be7beb480d878e21
santu001 <48280328+santu001@users.noreply.github.com>
2023-01-18 11:21:27 +0100

    Update Telegram.py

--- Notification_application/Telegram.py
+++ Notification_application/Telegram.py
@@ -6,10 +6,9 @@ class Main:
         base_url = "https://api.telegram.org/bot5928782166:AAF7TvP32JMgiyhXEDhZnCgE4LaGF48JFAk/sendPhoto"
         parameters = {
         "chat_id" : "-615882008",
-        "photo" : link,
         "caption" : message
         }
-        resp = requests.get(base_url, data = parameters)
+        resp = requests.post(base_url, data = parameters, files=link)
         print(resp.text)
 
         
