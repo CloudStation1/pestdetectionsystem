915de3577eece507850a4d9ea5c49a2655aae71e
santu001 <48280328+santu001@users.noreply.github.com>
2023-01-18 11:20:44 +0100

    Update Telegram_notifier.py

--- Notification_application/Telegram_notifier.py
+++ Notification_application/Telegram_notifier.py
@@ -1,6 +1,7 @@
 
 import Telegram
-a="https://github.com/CloudStation1/pestdetectionsystem/blob/1361ec75fea982105a83e63420fbfda6af61fb96/WebServer/Webapp/static/images/rat-image.jpeg"
+#a="https://github.com/CloudStation1/pestdetectionsystem/blob/1361ec75fea982105a83e63420fbfda6af61fb96/WebServer/Webapp/static/images/rat-image.jpeg"
+a={'photo':open(r'C:\Users\ADC\OneDrive\Pictures\Screenshots\Screenshot (73).png','rb')}
 b="This is Message"
 object=Telegram.Main()
 
