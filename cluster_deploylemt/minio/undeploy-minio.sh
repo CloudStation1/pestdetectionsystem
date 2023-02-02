135ede36ce340c62bbc2f7030b1de418485b61ef
sameer soni <indomitablesameer@gmail.com>
2023-02-03 00:09:08 +0100

    adding deployment files

--- cluster_deploylemt/minio/undeploy-minio.sh
+++
@@ -1,3 +0,0 @@
-kubectl delete deployment minio-deployment \
-&&  kubectl delete pvc minio-pv-claim \
-&& kubectl delete svc minio-service
---
+++ cluster_deploylemt/notifier/undeploy.sh
@@ -0,0 +1,1 @@
+sudo kubectl delete deployment notifier-deployment
